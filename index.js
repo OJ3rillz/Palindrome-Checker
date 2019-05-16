acaf620f71648c838dee6393feea47bd7a0e08c0
OJ3rillz <timitrilli@gmail.com>
2019-05-16 14:41:50 +0100

    add updates

--- index.js
+++ index.js
@@ -1,12 +1,27 @@
 function palindrome(str) {
     let letters = [];
-    let regix = ReqExp('', 'g');
+    let regix = ReqExp('[]', 'g');
 
 // remove special characters, while spaces and punctuation
 str = str.replace(regix, '');
+str = srt.toLowerCase();
 
-      return true;
-    }
+//create an array of letters
+for(let i = 0; i < str.length; i++) {
+      letters.push(str[i]);
+}
+
+//reverse the letters and join them to a string
+letters.reverse();
+let newStr = letters.join('');
+
+//check if newStr is the same as str
+if(str === newStr) {
+  return true;
+  } else {
+        return false;
+  }
+}
     
     
     
