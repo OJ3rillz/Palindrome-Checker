3b9a275c3fa2a33198e712f3393567284fcb0f4e
OJ3rillz <timitrilli@gmail.com>
2019-05-16 14:23:36 +0100

    Create index.js and add progress

---
+++ index.js
@@ -0,0 +1,13 @@
+function palindrome(str) {
+    let letters = [];
+    let regix = ReqExp('', 'g');
+
+// remove special characters, while spaces and punctuation
+str = str.replace(regix, '');
+
+      return true;
+    }
+    
+    
+    
+    palindrome("eye");
